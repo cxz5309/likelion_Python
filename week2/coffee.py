e4b5d30af2176c2b25ccd74aba0967cb8c4ca28f
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-08-24 21:39:08 +0900

    week3

--- week2/coffee.py
+++ week2/coffee.py
@@ -1,6 +1,23 @@
-money = input()
-menuNum = input('''1. 아아
+myDic = {
+    1: {'name': '아아', 'price': 1000, '': 1},
+    2: {'name': '따아', 'price': 2000, '': 2},
+    3: {'name': '라떼', 'price': 3000, '': 3},
+}
+
+while(True):
+    money = int(input('준 돈 : '))
+    menuNum = int(input('''1. 아아
 2. 따아
-3. 라떼''')
-myDic = {1: ['아아', 1000], 2: ['따아', 2000], 3: ['라떼', 3000]}
-print(f'{myDic[menuNum][0]}입니다! 거스름돈 {money - myDic[menuNum][1]}')
+3. 라떼
+주문 번호 : '''))
+    count = int(input('수량 : '))
+
+    if(myDic[menuNum]['stock'] - count >= 0):
+        print('{0} 주문! 거스름돈은 {1}원 입니다'
+              .format(myDic[menuNum]['name'], str(money - myDic[menuNum]['price'])))
+
+        myDic[menuNum]['stock'] -= count
+        print('{0} 개 남아있습니다'.format(myDic[menuNum]['stock']))
+
+    else:
+        print('주문불가!')
